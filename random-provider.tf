031eefe7a3c431f59565ada37554ba3d566e562c
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2023-04-11 09:54:16 +0300

    Update random-provider.tf

--- random-provider.tf
+++ random-provider.tf
@@ -1,18 +1,5 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-}
-
 resource "random_id" "number1byte" {
   byte_length = 8
-}
-
-output "id" {
-  value = "${random_id.number1byte.id}"
 }
 
   
